af508460b2687b6491cad99c4d58c7ddd3894881
vliedel <bart@crownstone.rocks>
2022-05-26 11:45:19 +0200

    version bump for release 1.2.0

--- bluenet_logs/BluenetLogs.py
+++ bluenet_logs/BluenetLogs.py
@@ -13,7 +13,7 @@ from bluenet_logs.LogFormatter import LogFormatter
 _LOGGER = logging.getLogger(__name__)
 
 class BluenetLogs:
-	__version__ = "1.1.0-git"
+	__version__ = "1.2.0"
 
 	def __init__(self):
 		self._logFormatter = LogFormatter()
